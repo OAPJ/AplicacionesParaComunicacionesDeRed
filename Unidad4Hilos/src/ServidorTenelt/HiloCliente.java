fa6e838c2e3214acad725a2bb734e90f7a9d1022
Juan Antonio <ovallejuan98@gmail.com>
2019-04-01 18:45:23 -0600

    Recibir mensaje del cliente con Telnet e hilos

--- Unidad4Hilos/src/ServidorTenelt/HiloCliente.java
+++ Unidad4Hilos/src/ServidorTenelt/HiloCliente.java
@@ -5,7 +5,9 @@
  */
 package ServidorTenelt;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -16,6 +18,7 @@ import java.net.Socket;
 public class HiloCliente extends Thread{
     private Socket cliente;
     private PrintWriter salida;
+    private BufferedReader entrada;
     
     public HiloCliente(Socket s){
         cliente = s;
@@ -26,6 +29,10 @@ public class HiloCliente extends Thread{
             salida = new PrintWriter(cliente.getOutputStream());
             System.out.println("Cliente: "+cliente.getInetAddress().getHostName()+" Puerto: "+cliente.getPort());
             salida.print("Hola cliente");
+            
+            entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+            System.out.println(entrada.readLine());
+            
             salida.close();
             cliente.close();
         } catch (IOException ex) {
